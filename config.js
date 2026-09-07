4a2f5a9e3d7e0a357db67d3fc6a211d0bc9be0f1
munifvector <akunbingart2@outlook.com>
2026-09-07 07:49:52 +0700

    Update Supabase URL in config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Supabase public configuration
 // Gunakan publishable/anon key saja di browser. Jangan gunakan service_role.
-window.SUPABASE_URL = "https://iciltedgxirvzymljqde.supabase.co";
+window.SUPABASE_URL = "https://ayqfiluswjfagvcjvyxs.supabase.co";
 window.SUPABASE_ANON_KEY = "sb_publishable_pn2UDDL9KEJ81IpCXVvfRQ_tNvXrLk2";
