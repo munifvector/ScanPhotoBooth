ffba67f1224065d5718a854ee8640d2390162e85
munifvector <akunbingart2@outlook.com>
2026-09-07 07:54:18 +0700

    Update Supabase configuration comments
    Removed comment about using service_role in browser.

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
 // Supabase public configuration
-// Gunakan publishable/anon key saja di browser. Jangan gunakan service_role.
 window.SUPABASE_URL = "https://ayqfiluswjfagvcjvyxs.supabase.co";
 window.SUPABASE_ANON_KEY = "sb_publishable_pn2UDDL9KEJ81IpCXVvfRQ_tNvXrLk2";
