d0a9f35d467c7be7dedec462d8165ce29a364b79
munifvector <akunbingart2@outlook.com>
2026-09-07 07:29:46 +0700

    Update Supabase ANON_KEY in config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Supabase public configuration
 // Gunakan publishable/anon key saja di browser. Jangan gunakan service_role.
 window.SUPABASE_URL = "https://iciltedgxirvzymljqde.supabase.co";
-window.SUPABASE_ANON_KEY = "sb_publishable_PE-qZEwk7APhnVjSoPk35g_l0rUobgo";
+window.SUPABASE_ANON_KEY = "sb_publishable_XpKEYeGewRadh800fkOcKQ_f4clTCPg";
